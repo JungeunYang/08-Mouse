7d1126d6dfcbe5b349c3dc75ff25237e57cc779f
jungeun <yangje1113@naver.com>
2023-12-12 15:35:16 +0900

    스크롤탑이 타겟포스에 도달하면 멈추게(오류)

--- 02-Mouse-move/js/script.js
+++ 02-Mouse-move/js/script.js
@@ -16,21 +16,19 @@ $(function () {
   let my = 0;
   let speed = 0.008;
 
+  // 반복되는 동작(moving)을 변수에 저장 (취소시키려고)
+  let movingObj;
+
   // 함수를 3개 만들기
   // 1. 마우스 좌표값 받아오는 함수
+  function getOffset() {
+    $window.on('mousemove', function (e) {
+      x = e.pageX - $window.outerWidth() / 2;
+      y = e.pageY - $window.outerHeight() / 2;
+    });
+  }
+
   // 2. 오브젝트를 움직이게 하는 함수
-  // 3. 1번과 2번을 실행시키는 함수
-
-  // 마우스가 움직일 때 좌표값 받아오기
-  $window.on('mousemove', function (e) {
-    x = e.pageX - $window.outerWidth() / 2;
-    y = e.pageY - $window.outerHeight() / 2;
-  });
-
-  let movingObj;
-  moving();
-
-  // 마우스의 기본 좌표값을 기준으로 어떤 값을 만들어 내자
   function moving() {
     mx += (x - mx) * speed;
     my += (y - my) * speed;
@@ -55,8 +53,29 @@ $(function () {
     movingObj = requestAnimationFrame(moving);
   }
 
-  $objWrap.on('click', function () {
-    cancelAnimationFrame(movingObj);
-    setTimeout(moving, 2000);
+  // 3. 1번과 2번을 실행시키는 함수
+  // function initMoving () {} -> 이름 있는 함수
+  // function () {} -> 무명 함수
+  const initMoving = function () {
+    getOffset();
+    moving();
+  };
+
+  initMoving();
+
+  // 스크롤 값이 콘텐츠 영역을 넘어가면
+  $window.on('scroll', function () {
+    let scrollTop = $(this).scrollTop();
+    let targetPos = $('.scroll-area').offset().top;
+    console.log(scrollTop, targetPos);
+
+    if (scrollTop >= targetPos) {
+      // 만약 스크롤값이 targetPos를 넘어가면
+      // 애니메이션을 멈추고
+      cancelAnimationFrame(movingObj);
+    } else {
+      // 그렇지 않으면 다시 애니메이션 실행
+      initMoving();
+    }
   });
 });
